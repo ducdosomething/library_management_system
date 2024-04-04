89b896d8bfe6bd682c1e9b1607851684f52780c2
ducdosomething <duclehoang211201@gmail.com>
2024-04-04 16:33:57 +0700

    After login success, lead to main librarian main menu about manegerment book

--- view/LoginView.java
+++ view/LoginView.java
@@ -15,7 +15,32 @@ public class LoginView {
         LoginSystem loginSystem = new LoginSystem(filePath);
 
         System.out.println("------------ Log in ------------");
-        loginSystem.checkLogin(filePath);
+        if (loginSystem.checkLogin(filePath)) {
+            System.out.println("------------ Management program ------------");
+            int choice;
+
+            do {
+                System.out.println("Choose an option: ");
+                System.out.println("1. Show all books.");
+                System.out.println("2. Search book.");
+                System.out.println("0. Exit");
+                System.out.print("Enter your choice: ");
+                choice = scanner.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("Case 1");
+                        break;
+                    case 2:
+                        System.out.println("Case 2");
+                        break;
+                    case 0:
+                        System.out.println("Exiting...");
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            } while (choice != 3);
+        }
         System.out.println("Press 0 to return to the main menu.");
         int choice = scanner.nextInt();
         if (choice == 0) {
